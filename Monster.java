18474bffbb9fe9b6ea69960e50acb8bc9eedf73b
hizumi00 <81341477+hizumi00@users.noreply.github.com>
2021-06-10 15:41:20 +0900

    Update Monster.java
    修正
    ・getNameをabstractに

--- Monster.java
+++ Monster.java
@@ -4,18 +4,15 @@ public abstract class Monster {
 	int hp;
 	// 名前を取得する(getter)
 	// インスタンス作成時にsuffixを設定しているのでsetterはなし
-	public String getName;
+	public abstract String getName();
 	// HPを取得する(getter)
 	public int getHp() {
 		return this.hp;
 	}
 	// HPを設定する(setter)
-	public void setHP(int hp) {
+	public void setHp(int hp) {
 		if(hp > 10) {// 初期値は超えない
 			throw new IllegalArgumentException("over max hp");
-		}
-		if(hp < 0) {// マイナスの値は入れない
-			throw new IllegalArgumentException("under zero");
 		}
 		this.hp = hp;
 	}
