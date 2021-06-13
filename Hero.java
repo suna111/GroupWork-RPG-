94eced98f103fc48a7bbdcfbb420edf1ec5ef517
noborikawa <81341098+noborikawa@users.noreply.github.com>
2021-06-14 07:56:56 +0900

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,3 +1,10 @@
-public class Hero {
-	
+public class Hero extends Character {
+	String name;
+	int hp = 300;
+	final int MAX_HP = 300;
+	int mp = 50;
+	final int MAX_MP = 50;
+	public void Attack(Monster m) {
+		m.hp = -10;
+	}
 }
