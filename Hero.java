a91be7b5d211e48cf1b6c4da48d92961e6ac9e36
noborikawa <81341098+noborikawa@users.noreply.github.com>
2021-06-16 17:54:47 +0900

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,3 +1,34 @@
 public class Hero {
+		private String name; //名前
+	private int hp; //HP
+	final int MAX_HP = 300; //最大HP
+	private int mp; //MP
+	final int MAX_MP = 50; //最大MP
+
+	public String getName() {
+		return this.name;
+	} /*名前のgetter*/
+	public void setName(String name) {
+		this.name = name;
+	} /*名前のsetter*/
+
+	public Hero() {
+		this.setHp(300);
+		this.setMp(50);
+	}　/*コンストラクタ*/
+
+	public void Attack(Monster m) {
+		m.hp -= 10;
+	} /*戦う*/
 	
+	public void Aid(Heroh h) {
+		h.hp += 10;
+		this.mp -= 5;
+		System.out.println(h.hp);
+	} /*回復*/
+	
+	public void Thunder(Monster m) {
+		m.hp -= 30;
+		this.mp -= 15;
+	} /*雷斬り*/
 }
