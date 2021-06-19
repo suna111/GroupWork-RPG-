cffc6667e987a7fdf052c3e9db6bd059b3e3f423
noborikawa <81341098+noborikawa@users.noreply.github.com>
2021-06-19 14:50:22 +0900

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,34 +1,36 @@
-public class Hero {
-		private String name; //名前
+public class Hero extends Character {
+	//private String name; //名前
 	private int hp; //HP
 	final int MAX_HP = 300; //最大HP
 	private int mp; //MP
 	final int MAX_MP = 50; //最大MP
 
-	public String getName() {
-		return this.name;
-	} /*名前のgetter*/
-	public void setName(String name) {
-		this.name = name;
-	} /*名前のsetter*/
-
 	public Hero() {
 		this.setHp(300);
 		this.setMp(50);
-	}　/*コンストラクタ*/
+	}
 
-	public void Attack(Monster m) {
-		m.hp -= 10;
+	public void attack(monster m) {
+		int damage = 10;
+		m.setHp(m.getHp() - damage);
+		System.out.println("勇者は" + damage + "のダメージを与えた。");
 	} /*戦う*/
 	
-	public void Aid(Heroh h) {
+	public void aid(hero h) {
 		h.hp += 10;
 		this.mp -= 5;
 		System.out.println(h.hp);
 	} /*回復*/
-	
-	public void Thunder(Monster m) {
+	public void aid(wizard w) {
+		w.hp += 10;
+		this.mp -= 5;
+		System.out.println(w.hp);
+	} /*回復*/	
+
+	public void thunder(monster m) {
 		m.hp -= 30;
 		this.mp -= 15;
+		System.out.println(m.hp);
+		System.out.println(h.mp);
 	} /*雷斬り*/
 }
