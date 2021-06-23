32b95468e88acfc382360f6086ed097bfc9d52da
h-maya <85472592+h-maya@users.noreply.github.com>
2021-06-23 16:33:57 +0900

    Update Dragon.java

--- Dragon.java
+++ Dragon.java
@@ -1,3 +1,48 @@
-public class Dragon {
-	
-}
+public class Dragon extends Monster {
+	// 属性・フィールド 
+	private int Hp;// 初期値
+	final int MAX_HP=500;
+	public Dragon(char suffix) {// コンストラクタ
+		this.suffix = suffix;
+		this.setHp(500);
+	}
+	// Monster.getNameのオーバーライド
+	public String getName() {
+		return "ドラゴン" + this.suffix;
+	}
+	// 操作・メソッド 
+	// キャラクターの固有メソッド
+		// 攻撃1 炎の息(10のダメージ)
+		public void attack(Character c) throws Exception {// キャラクタークラスを継承しているものすべてに適応
+			// 内部処理
+			int damage = 10;
+			c.setHp(c.getHp() - damage);
+			// 出力処理
+			String[] comment = {
+				getName()+ "炎の息！",
+				c.getName() + "に10のダメージ！",
+				c.getName() + "のHPは残り" + c.getHp()
+			};
+            for(String cm : comment) {
+				System.out.println(cm);
+				Thread.sleep(1000);
+			}
+		}
+		// 攻撃2 噛みつき(20のダメージ)
+		public void bite(Character c) throws Exception {
+			int damage = 20;
+			c.setHp(c.getHp() - damage);
+			//出力処理
+			String[] comment = {
+				getName() + "が噛み付いた"
+			};
+			for(String cm : comment) {
+				System.out.println(cm);
+				Thread.sleep(1000);
+			}
+		}
+		// 逃げる
+		public void run() {
+			System.out.println( getName() + "は逃げた");
+		}
+	}
